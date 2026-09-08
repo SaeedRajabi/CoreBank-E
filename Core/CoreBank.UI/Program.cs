879186ca356bcb6d48a70bd196d9b1825c0a7108
saeedRajabi <arkasoftware1@gmail.com>
2026-09-08 12:30:36 +0330

    refactor(Add SplashForm and delete Form1)

--- Core/CoreBank.UI/Program.cs
+++ Core/CoreBank.UI/Program.cs
@@ -1,3 +1,5 @@
+using CoreBank.UI.Forms;
+
 namespace CoreBank.UI
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace CoreBank.UI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new SplashForm());
         }
     }
 }
